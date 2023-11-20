a9385cd07a9f61e0373c980df2a16ef0c7748913
Đạt <77010056+shirokuma-desu@users.noreply.github.com>
2023-11-20 17:59:57 +0700

    Add framwork observe pattern

---
+++ My project/Assets/Scripts/Utilities/Observer/EventID.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum EventID
+{
+    None = 0,
+    OnRerolledShop,
+    OnBuyingItem,
+    OnSellingItem,
+    OnReclaimUpgrade,
+}
