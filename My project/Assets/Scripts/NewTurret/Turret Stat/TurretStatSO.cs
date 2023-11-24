1fc39f76b07a5e0b6c9ae0200dce63d4ca8b992a
hoepjhsha <hiepnguyen3624@gmail.com>
2023-11-24 09:54:24 +0700

    Del sth unnecessary and Add sth about SO

---
+++ My project/Assets/Scripts/NewTurret/Turret Stat/TurretStatSO.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "TurretSO", menuName = "Scriptable Objects/Stat/TurretStat", order = 1)]
+public class TurretStatSO : ScriptableObject
+{
+    public int health;
+    public int attackDamage;
+    public float attackSpeed;
+    public float attackRange;
+}
