10c7caf0d536ea8c60475aecb640c68f7c94c4e8
Đạt <77010056+shirokuma-desu@users.noreply.github.com>
2023-11-25 12:29:45 +0700

    Update SkillObjectSO.cs

--- My project/Assets/Scripts/Shop/SkillObjectSO.cs
+++ My project/Assets/Scripts/Shop/SkillObjectSO.cs
@@ -7,9 +7,11 @@ public class SkillObjectSO : ScriptableObject
 {
     public int ID_Skill;
     public string skill_name;
+    public int level_skill;
     public Sprite image;
     public Sprite image_upgrade;
     public int price;
+    public int price_increasement;
     public int sellprice;
     public string description;
     public bool is_upgraded;
