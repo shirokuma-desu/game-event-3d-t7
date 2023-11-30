ec52c6dc512b2b52cc80f6f23b8caf43ef6c3219
Miaan <nghiabn2005@gmail.com>
2023-12-01 00:26:46 +0700

    Add+Fix Multicast

--- My project/Assets/Scripts/Skillver2/HandleFloatingText.cs
+++ My project/Assets/Scripts/Skillver2/HandleFloatingText.cs
@@ -9,6 +9,15 @@ public class HandleFloatingText : MonoBehaviour
     [SerializeField] private GameObject multicastTextPrefab;
     [SerializeField] private GameObject xTextPrefab;
 
+    [SerializeField]
+    private Vector3 m_effectPosition;
+    [SerializeField]
+    private GameObject m_normalMulticastEffect;
+    [SerializeField]
+    private GameObject m_x3MulticastEffect;
+    [SerializeField]
+    private GameObject m_x4MulticastEffect;
+
     private GameObject xText;
     private GameObject multicastText;
 
@@ -16,6 +25,8 @@ public class HandleFloatingText : MonoBehaviour
     private Animation multicastAnimation;
 
     private int multicastTimes;
+
+    private int m_currentMulticast;
 
     private void Start()
     {
@@ -34,6 +45,8 @@ public class HandleFloatingText : MonoBehaviour
             multicastText.SetActive(false);
             multicastAnimation = multicastText.GetComponent<Animation>();
         }
+
+        m_currentMulticast = 0;
     }
 
     public void ShowFloatingText()
@@ -42,23 +55,16 @@ public class HandleFloatingText : MonoBehaviour
 
         if (multicastTimes > 0 && multicastTimes <= 4)
         {
+            IEnumerator _showTextCoroutine = ShowText();
 
-            StartCoroutine(ShowText());
+            if (multicastTimes >= m_currentMulticast) StopAllCoroutines();
+            StartCoroutine(_showTextCoroutine);
         }
     }
 
     private IEnumerator ShowText()
     {
-        if (multicastText.activeSelf && xText.activeSelf)
-        {
-            xAnimation.Play("x_exit");
-            multicastAnimation.Play("text_exit");
-
-            yield return new WaitForSeconds(0.2f);
-
-            xText.SetActive(false);
-            multicastText.SetActive(false);
-        }
+        m_currentMulticast = multicastTimes;
 
         multicastText.SetActive(true);
         multicastAnimation.Play("text_enter");
@@ -73,21 +79,35 @@ public class HandleFloatingText : MonoBehaviour
                 yield return new WaitForSeconds(0.13f);
             }
 
-            xText.GetComponent<TextMeshPro>().text = "x" + i.ToString();
+            xText.GetComponent<TextMeshPro>().text = "x" + (i + 1).ToString();
             xAnimation.Play("x_enter");
+
+            if (i < multicastTimes)
+            {
+                Instantiate(m_normalMulticastEffect, m_effectPosition, Quaternion.identity);
+            }
+            else
+            {
+                if (i == 3) Instantiate(m_x3MulticastEffect, m_effectPosition, Quaternion.identity);
+                else if (i == 4) Instantiate(m_x4MulticastEffect, m_effectPosition, Quaternion.identity);
+                else Instantiate(m_normalMulticastEffect, m_effectPosition, Quaternion.identity);
+            }
+
             yield return new WaitForSeconds(0.2f);
         }
 
-
         yield return new WaitForSeconds(1f);
-
+        if (m_currentMulticast >= 3) yield return new WaitForSeconds(.5f);
+        if (m_currentMulticast >= 4) yield return new WaitForSeconds(.25f);
 
         xAnimation.Play("x_exit");
         multicastAnimation.Play("text_exit");
 
         yield return new WaitForSeconds(0.13f);
-
+        
         xText.SetActive(false);
         multicastText.SetActive(false);
+
+        m_currentMulticast = 0;
     }
 }
