810c63607a7ddda924ef7389f1b5d6db1d091d45
Đạt <77010056+shirokuma-desu@users.noreply.github.com>
2023-12-03 18:28:32 +0700

    update SO data

--- My project/Assets/Scripts/Shop/ScriptableObj/ItemDataSO.cs
+++ My project/Assets/Scripts/Shop/ScriptableObj/ItemDataSO.cs
@@ -5,15 +5,40 @@ using UnityEngine;
 [CreateAssetMenu(fileName ="SkillObjSO",menuName ="Scriptable Objects/Shop/Item",order = 1)]
 public class ItemDataSO : ScriptableObject
 {
-    public int      ID_Skill;
-    public string   skill_name;
-    public int      level_skill;
-    public Sprite   image;
-    public Sprite   image_upgrade;
-    public int      price;
-    public int      price_increase;
-    public int      sell_price_increase;
-    public int      sellprice;
-    public string   description;
-    public bool     is_upgraded;
+    [Header("ID")]
+    public int ID_Skill;
+    [Header("UI")]
+    public Sprite image;
+    public Sprite image_upgrade;
+    public string skill_name;
+    public string description;
+    public int    multi_cast_chance;
+    [Header("Currency Stats")]
+    public int price;
+    public int sellprice;
+    public int price_increase;
+    public int sell_price_increase;
+    [Header("Common Stats")]
+    public int level_skill;
+    public int damage;
+    [Header("Skill Stats")]
+    public int radius;
+    public float cooldown;
+    public int instance_per_cast;
+    public float debuff_duration;
+    public float debuff_effective;
+    [Header("Upgrade Stats Per Level")]
+    public int damage_increase;
+    public int radius_increase;
+    public float cooldown_decrease;
+    public int instance_increase;
+    public float debuff_duration_increase;
+    public float debuff_effective_increase;
+    [Header("Turret Stats Per Level")]
+    public int range_increase;
+    public int fire_rate_increase;
+    public int hp_increase;
+    [Header("Mics Stats")]
+    public bool is_upgraded;
+    
 }
