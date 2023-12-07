92d388c2b7f7b0ee66a9c00eb44dc1bca5ea7494
Miaan <nghiabn2005@gmail.com>
2023-12-07 09:07:49 +0700

    Add basic of Game Manager

--- My project/Assets/Scripts/GameManager/GameManager.cs
+++ My project/Assets/Scripts/GameManager/GameManager.cs
@@ -4,7 +4,37 @@ using UnityEngine;
 
 public class GameManager : GenericSingleton<GameManager>
 {
+    public enum State
+    {
+        Running,
+        Paused
+    }
+
+    [Header("References")]
     [SerializeField]
     private ShopSystem m_shop;
     public ShopSystem Shop { get => m_shop; }
+    
+    private State m_gameState;
+    public State GameState { get => m_gameState; }
+
+    public void PauseGame()
+    {
+        if (m_gameState == State.Running) 
+        {
+            Debug.LogWarning("GameManager: The game is already paused");
+        }
+
+        m_gameState = State.Paused;
+    }
+
+    public void ResumeGame()
+    {
+        if (m_gameState == State.Running) 
+        {
+            Debug.LogWarning("GameManager: The game is already running");
+        }
+
+        m_gameState = State.Running;
+    }
 }
