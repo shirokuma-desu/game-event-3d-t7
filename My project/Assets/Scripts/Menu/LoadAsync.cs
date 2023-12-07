db2139cda78324ca23469c9ca25e7dce1ae8e120
Miaan <nghiabn2005@gmail.com>
2023-12-07 23:57:03 +0700

    Add loading scene to start menu

---
+++ My project/Assets/Scripts/Menu/LoadAsync.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoadAsync : GenericSingleton<LoadAsync>
+{
+    [SerializeField]
+    private Slider m_loadingBar;
+
+    [SerializeField]
+    private Animator m_animator;
+
+    public void LoadScene(string _sceneName)
+    {
+        StartCoroutine(LoadSceneCoroutine(_sceneName));
+    }
+
+    private IEnumerator LoadSceneCoroutine(string _sceneName)
+    {
+        m_animator.SetBool("Start", true);
+
+        yield return new WaitForSeconds(2f);
+
+        AsyncOperation _loadOperation = SceneManager.LoadSceneAsync(_sceneName);
+
+        while (!_loadOperation.isDone)
+        {
+            float _progress = Mathf.Clamp01(_loadOperation.progress / .9f);
+            m_loadingBar.value = _progress;
+
+            yield return new WaitForEndOfFrame();
+        }
+
+        yield return new WaitForSeconds(.5f);
+
+        m_animator.SetBool("End", true);
+
+        yield return null;
+    }
+}
---
+++ My project/Assets/Scripts/Tutorial/TutorialManager.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TutorialManager : MonoBehaviour
+{
+    [SerializeField]
+    private Image m_imageShow;
+
+    [SerializeField]
+    private Sprite[] m_images;
+
+    private int m_currentImage;
+    private int m_imageNumber;
+
+    private void Start()
+    {
+        m_imageNumber = m_images.Length;
+        m_currentImage = 0;
+
+        m_imageShow.sprite = m_images[m_currentImage];
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Return) || Input.GetMouseButtonDown(0))
+        {
+            m_currentImage++;
+
+            if (m_currentImage >= m_imageNumber)
+            {
+                PlayGame();
+
+                return;
+            }
+
+            m_imageShow.sprite = m_images[m_currentImage];
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            if (m_currentImage > 0) m_currentImage--;
+
+            m_imageShow.sprite = m_images[m_currentImage];
+        }
+    }
+
+    private void PlayGame()
+    {
+        Debug.Log("PLAY!");
+    }
+}
