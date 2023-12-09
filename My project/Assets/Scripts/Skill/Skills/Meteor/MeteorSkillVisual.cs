e3b212e7fd5f5d3cf1063183c3cffa17e2ff0501
Miaan <nghiabn2005@gmail.com>
2023-12-09 16:02:23 +0700

    Fix skill scale

--- My project/Assets/Scripts/Skill/Skills/Meteor/MeteorSkillVisual.cs
+++ My project/Assets/Scripts/Skill/Skills/Meteor/MeteorSkillVisual.cs
@@ -41,8 +41,8 @@ public class MeteorSkillVisual : SkillVisual
         m_previewObject.transform.position = _position;
 
         Vector3 _scale = m_previewObject.transform.localScale;
-        _scale.x = m_skillData.Range;
-        _scale.y = m_skillData.Range;
+        _scale.x = m_skillData.Range / 4f;
+        _scale.z = m_skillData.Range / 4f;
         m_previewObject.transform.localScale = _scale;
     }
 
